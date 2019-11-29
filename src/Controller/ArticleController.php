134bbabd782cf2fca48a4b679ac940fafa2ca7aa
gaurav <gauravnikki@gmail.com>
2019-11-29 15:17:41 +0100

    initial commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,39 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('This is my first page ! Woo');
+    }
+
+    /** 
+     * @Route("/news/{slug}")
+     */
+
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig',[
+           'title' => ucwords(str_replace('-','',$slug)),
+           'comments'=> $comments,
+        ]);
+    }
+}
